260d29c07cc5d0c73df1797ec77ccab36c986e2f
CallmePapa <1062927638@qq.com>
2018-04-22 15:12:13 +0800

    4.21

--- 4.21/lintCode4.21.js
+++ 4.21/lintCode4.21.js
@@ -1,12 +1,14 @@
 function reserve(str,target){
+    let len=str.length;
     if(target===0){
         return;
     }
-    if(str.length===0){
+    if(len===0){
         return;
     }
-    let strAt=str.slice(target,target+1);
+    let strAt=str.slice(len-target,len);
     let newStr=str.split(strAt);
-    let result=newStr[1].concat(newStr[0]);
+    let result=strAt.concat(newStr[0]);
+    return result;
 }
 console.log(reserve("absdcfgd",3));
