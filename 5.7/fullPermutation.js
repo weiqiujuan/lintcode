f9ed81a37fa801c509e60f080790b1afc32795e6
CallmePapa <1062927638@qq.com>
2018-05-07 06:25:23 +0800

    :sparkles:

--- 5.7/fullPermutation.js
+++ 5.7/fullPermutation.js
@@ -1,28 +1,29 @@
 function permutation(nums) {
 
-    if(nums===null) return [];
-    if(nums.length===0) return [[]];
-    let result=[];
-    let subRes=[];
-    let set=new Set();
+    if (nums === null) return [];
+    if (nums.length === 0) return [[]];
+    let result = [];
+    let subRes = [];
+    let set = new Set();
 
-    judge(nums,subRes,set,result);
+    judge(nums, subRes, set, result);
 
     return result;
 
 }
-function judge(nums,subRes,set,result){
 
-    if(subRes.length===nums.length){
+function judge(nums, subRes, set, result) {
+
+    if (subRes.length === nums.length) {
         result.push(subRes.concat([]));
     }
 
-    let len=nums.length;
-    for(let i=0;i<len;i++){
-        if(!set.has(nums[i])){
+    let len = nums.length;
+    for (let i = 0; i < len; i++) {
+        if (!set.has(nums[i])) {
             set.add(nums[i]);
             subRes.push(nums[i]);
-            judge(nums,subRes,set,result);
+            judge(nums, subRes, set, result);
             subRes.pop();
             set.delete(nums[i]);
         }
