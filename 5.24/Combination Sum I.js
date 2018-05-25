b4d38fbcd2b867c9bcfe2c75982d2d2ba1a23a9a
CallmePapa <1062927638@qq.com>
2018-05-25 10:52:03 +0800

    :sparkles:

---
+++ 5.24/Combination Sum I.js
@@ -0,0 +1,42 @@
+let combinationSum = function (candidates, target) {
+    if (candidates === null || candidates.length === 0) {
+        return [];
+    }
+    //先排序
+    candidates.sort((num1, num2) => {
+        return num1 - num2;
+    });
+
+    let result = [];
+    let combination = [];
+
+    //递归
+    dfs(candidates, 0, target, combination, result);
+
+    return result;
+};
+
+const dfs = (candidates, startIndex, target, combination, result) => {
+
+    //找到最后的target组合数组
+    if (target === 0) {
+        result.push(combination.concat([]));
+    }
+
+    let size = candidates.length;
+
+    for (let i = startIndex; i < size; i++) {
+        if (candidates[i] > target) {
+            break;
+        }
+
+        if (i === 0 || candidates[i] !== candidates[i - 1] || i === startIndex) {
+            combination.push(candidates[i]);
+            dfs(candidates, i + 1, target - candidates[i], combination, result);
+            combination.pop();
+        }
+    }
+};
+
+let test = [2, 3, 6, 7];
+console.log(combinationSum([2, 3, 6, 7], 8));
