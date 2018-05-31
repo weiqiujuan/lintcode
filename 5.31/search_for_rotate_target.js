0692f18729980d1288457378513347a80618152b
CallmePapa <1062927638@qq.com>
2018-05-31 17:29:42 +0800

    :sparkles:

---
+++ 5.31/search_for_rotate_target.js
@@ -0,0 +1,40 @@
+const search = (nums, target) => {
+    if (nums === null || nums.length === 0 || target === undefined) {
+        return -1;
+    }
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start + 1 < end) {
+        let mid = Math.floor(start + (end - start) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+        if (nums[start] < nums[mid]) {
+            if (nums[start] <= target && target <= nums[mid]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        else {
+            if (nums[mid] <= target && target <= nums[end]) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+    }
+    if (nums[start] === target) {
+        return start;
+    }
+    if (nums[end] === target) {
+        return end;
+    }
+    return -1;
+};
+
+let test=[4, 5, 1, 2, 3];
+let target=1;
+console.log(search(test,target));
