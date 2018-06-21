baa5c3fe0ef7ba3a72041aee18b9045f562ff387
CallmePapa <1062927638@qq.com>
2018-06-21 14:12:00 +0800

    first

---
+++ 6.21/copyRandomList.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * function RandomListNode(label) {
+ *     this.label = label;
+ *     this.next = this.random = null;
+ * }
+ */
+
+/**
+ * @param {RandomListNode} head
+ * @return {RandomListNode}
+ */
+let copyRandomList = function(head) {
+
+
+};
